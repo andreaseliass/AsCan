cb714a18a84524c37ff0bec2d3c1eed85d2a4077
Andrea Elias <andreaase@hotmail.com>
2022-08-04 14:39:01 -0300

    home final

--- Trilha Frontend/curso React/star-war-app/src/components/pages/Home.jsx
+++ Trilha Frontend/curso React/star-war-app/src/components/pages/Home.jsx
@@ -2,24 +2,27 @@ import { Link } from 'react-router-dom'
 import filmes from '../../img/movies.png'
 import people from '../../img/people.webp'
 import planets from '../../img/planets.webp'
-import species from '../../img/species.jpg'
+import species from '../../img/species.png'
+import starships from '../../img/starships.webp'
+import vehicles from '../../img/vehicles.jpg'
 import styles from './Paginas.module.css'
 
 function Home(){
     return (
     <> 
-         <Link to="/films">
             <div className={styles.card}>
-                <img src={filmes} alt="Filmes" width='360px' height='350px'></img>
-                <div className={styles.containerhome}>
-                    <h2><b>Filmes</b></h2>
-                </div>
+                <Link to="/films" style={{ textDecoration: 'none' }}>
+                    <img src={filmes} alt="Filmes" width='360px' height='350px'></img>
+                    <div className={styles.containerhome}>
+                        <h2><b>Filmes</b></h2>
+                    </div>
+                </Link>
             </div>
-         </Link>
         
         
-        <div className={styles.card}>
-            <Link to="/films">
+        
+        <div className={styles.card} >
+            <Link to="/people" style={{ textDecoration: 'none' }}>
                 <img src={people} alt="Filmes" width='360px' height='350px' text-align='center'></img>
                 <div className={styles.containerhome}>
                     <h2><b>Personagens</b></h2>
@@ -29,32 +32,39 @@ function Home(){
         </div>
 
         <div className={styles.card}>
-            <img src={planets} alt="Filmes" width='360px' height='350px' onclick="location.href='/films';"></img>
-            <div className={styles.containerhome}>
-                <h2><b>Planetas</b></h2>
-            </div>
+            <Link to="/planets" style={{ textDecoration: 'none' }}>
+                <img src={planets} alt="Filmes" width='360px' height='350px'></img>
+                <div className={styles.containerhome}>
+                    <h2><b>Planetas</b></h2>
+                </div>
+            </Link>
 
         </div>
         <div className={styles.card}>
-            <img src={species} alt="Filmes" width='360px' height='350px'></img>
-            <div className={styles.containerhome}>
-                <h2><b>Espécies</b></h2>
-            </div>
-
+            <Link to="/species" style={{ textDecoration: 'none' }}>
+                <img src={species} alt="Filmes" width='360px' height='350px'></img>
+                <div className={styles.containerhome}>
+                    <h2><b>Espécies</b></h2>
+                </div>
+            </Link>
         </div>
 
         <div className={styles.card}>
-            <img src={filmes} alt="Filmes" width='360px' height='350px'></img>
-            <div className={styles.containerhome}>
-                <h2><b>Naves</b></h2>
-            </div>
-
+            <Link to="/starships" style={{ textDecoration: 'none' }}>
+                <img src={starships} alt="Filmes" width='360px' height='350px'></img>
+                <div className={styles.containerhome}>
+                    <h2><b>Naves</b></h2>
+                </div>
+            </Link>
         </div>
+
          <div className={styles.card}>
-            <img src={filmes} alt="Filmes" width='360px' height='350px'></img>
+         <Link to="/vehicles" style={{ textDecoration: 'none' }}>
+            <img src={vehicles} alt="Filmes" width='360px' height='350px'></img>
             <div className={styles.containerhome}>
                 <h2><b>Veículos</b></h2>
             </div>
+        </Link>
 
         </div>
     </>  
